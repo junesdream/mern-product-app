bae184b3f5e55e561a1ba3dfa59539143339a925
junesdream <norisori@gmail.com>
2024-10-09 02:24:06 +0200

    Refactor server.js to add API endpoints for fetching, creating, updating, and deleting products

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
-
-
 app.use(express.json()); // middleware
+
+app.get("/api/products", async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.status(200).json({ success: true, data: products });
+	} catch (error) {
+		console.error("Error in Fetch Products:", error.message);
+		res.status(500).json({ success: false, message: "Server error" });
+	}
+})
 
 app.post("/api/products", async (req, res) => {
 	const product = req.body;
@@ -29,6 +38,37 @@ app.post("/api/products", async (req, res) => {
 		console.error("Error in Create Product:", error.message);
 		res.status(500).json({ success: false, message: "Server error" });
 	}
+});
+
+app.put("/api/products/:id", async(req, res) => {
+	const { id } = req.params;
+	const product = req.body;
+
+	console.log("Received body:", product);
+
+	if(!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(404).json({ success: false, message: "Invalid product id" });
+	}
+
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+		res.status(200).json({ success: true, data: updatedProduct });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Server error" });
+	}
+})
+
+app.delete("/api/products/:id", async(req, res) => {
+	 const { id } = req.params;
+	
+
+	 try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({ success: true, message: "Product removed" });
+		} catch (error) {
+		console.error("Error in Fetch Products:", error.message);
+		res.status(404).json({success:false, message: "Product not found"});
+		}
 });
 
 app.listen(5001, () => {
